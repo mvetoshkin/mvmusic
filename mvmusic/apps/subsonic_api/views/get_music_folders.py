c96aa1e88b28d17aa487c3e35424a77517da0a92
Mikhail Vetoshkin <mikhail@vetoshkin.co>
2022-01-06 01:16:03 +0300

    use the user_libraries argument in the get music folders view

--- mvmusic/apps/subsonic_api/views/get_music_folders.py
+++ mvmusic/apps/subsonic_api/views/get_music_folders.py
@@ -6,9 +6,7 @@ class GetMusicFoldersView(BaseView):
     def process_request(self):
         return {
             'musicFolders': {
-                'musicFolder': [
-                    library_serializer(i)
-                    for i in self.current_user.libraries
-                ]
+                'musicFolder': [library_serializer(i)
+                                for i in self.user_libraries]
             }
         }
