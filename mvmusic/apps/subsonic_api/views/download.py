347c04594c4394e0cbdb496ea30e679575e3774c
Mikhail Vetoshkin <mikhail@vetoshkin.co>
2022-01-13 18:30:23 +0300

    raise an exception if user doesn't have access to media

--- mvmusic/apps/subsonic_api/views/download.py
+++ mvmusic/apps/subsonic_api/views/download.py
@@ -2,6 +2,7 @@ import os
 
 from flask import send_file
 
+from mvmusic.libs.exceptions import AccessDeniedError
 from mvmusic.models.library import Library
 from mvmusic.models.media import Media
 from . import BaseView
@@ -11,5 +12,9 @@ class DownloadView(BaseView):
     def process_request(self, id_):
         media = Media.query.get(id_)
         library = Library.query.get(media.library_id)
+
+        if library not in self.user_libraries:
+            raise AccessDeniedError
+
         file_path = os.path.join(library.path, media.path)
         return send_file(file_path, mimetype=media.content_type)
